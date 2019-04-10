8a40b00d517057af26743ddd48409d35037ed003
MarcinBalejko <mbbm9@o2.pl>
2019-04-10 23:30:19 +0800

    Refactored

--- directory.rb
+++ directory.rb
@@ -2,17 +2,13 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
 students = []
-# get the first name
 name = gets.chomp
-# while the name is not empty, repeat this code
  while !name.empty? do
    students << {name: name, cohort: :november}
    puts "now we have #{students.count} students"
    name = gets.chomp
  end
- # return the array of students
 students
 end
 def print_header
