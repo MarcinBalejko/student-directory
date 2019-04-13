4fe32d000047705421289877de53730e8e2ad35c
MarcinBalejko <mbbm9@o2.pl>
2019-04-13 20:59:44 +0800

    Add cohort prompt and enumeration

--- directory.rb
+++ directory.rb
@@ -1,13 +1,17 @@
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 students = []
 name = gets.chomp
+  puts "Please enter the cohort"
+cohort_prompt = gets.chomp
  while !name.empty? do
-   students << {name: name, cohort: :november}
+   students << {name: name, cohort: cohort_prompt}
    puts "now we have #{students.count} students"
+   puts "Please enter the name of the student"
    name = gets.chomp
+   puts "Please enter the cohort"
+   cohort_prompt = gets.chomp
  end
 students
 end
@@ -16,12 +20,13 @@ def print_header
   puts "-------------"
 end
 def print(students)
-students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, index|
+  indexplusone = index + 1
+  puts "#{indexplusone}. #{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
 end
 students = input_students
 print_header
