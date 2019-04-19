66afb1df27dd89b54711993082c09d9e4b965768
MarcinBalejko <mbbm9@o2.pl>
2019-04-20 01:17:37 +0800

    Made methods shorter and refactored

--- directory.rb
+++ directory.rb
@@ -1,65 +1,66 @@
+@students = []
 def interactive_menu
-students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+def process(selection)
+  case selection
   when "1"
-    students = input_students
+    @students = input_students
   when "2"
     print_header
-    print(students)
-    print_footer(students)
+    print_students_list
+    print_footer
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
     end
   end
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-students = []
 name = gets.chomp
   puts "Please enter the cohort"
 cohort_prompt = gets.chomp
  while !name.empty? do
-   students << {name: name, cohort: cohort_prompt}
-   if students.length == 1
-     puts "Now we have #{students.count} student"
+   @students << {name: name, cohort: cohort_prompt}
+   if @students.length == 1
+     puts "Now we have #{@students.count} student"
      else
-       puts "Now we have #{students.count} students"
+       puts "Now we have #{@students.count} students"
      end
    puts "Please enter the name of the student"
    name = gets.chomp
    puts "Please enter the cohort"
    cohort_prompt = gets.chomp
  end
-students.sort_by {|sym| sym[:cohort]}
+@students.sort_by {|sym| sym[:cohort]}
 end
 def print_header
   puts "The students of Villains Academy"
   puts "--------------------------------"
 end
-def print(students)
-students.each_with_index do |student, index|
+def print_students_list
+@students.each_with_index do |student, index|
   indexplusone = index + 1
   puts "#{indexplusone}. #{student[:name]} (#{student[:cohort]} cohort)".center(30)
 end
 end
-def print_footer(students)
+def print_footer
   puts "--------------------------------"
-  puts "Overall, we have #{students.count} great students."
+  puts "Overall, we have #{@students.count} great students."
 end
 interactive_menu
-students = input_students
-if students.length != 0
-print_header
-print(students)
-print_footer(students)
-else
-  interactive_menu
-end
