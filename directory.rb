6986427007836fa912c14813c02e18cd9fb80e3e
MarcinBalejko <mbbm9@o2.pl>
2019-04-21 00:15:25 +0800

    Add save list option

--- directory.rb
+++ directory.rb
@@ -1,4 +1,13 @@
 @students = []
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+file.close
+end
 def interactive_menu
   loop do
     print_menu
@@ -8,6 +17,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 def process(selection)
@@ -18,6 +28,8 @@ def process(selection)
     print_header
     print_students_list
     print_footer
+  when "3"
+    save_students
   when "9"
     exit
   else
