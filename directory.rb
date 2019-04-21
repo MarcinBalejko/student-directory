4ee7416d3dbe47058f64ed222e3890872b58b15f
MarcinBalejko <mbbm9@o2.pl>
2019-04-21 23:28:06 +0800

    Add load list from file

--- directory.rb
+++ directory.rb
@@ -8,6 +8,14 @@ def save_students
   end
 file.close
 end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort}
+  end
+  file.close
+end
 def interactive_menu
   loop do
     print_menu
@@ -18,6 +26,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 def process(selection)
@@ -30,6 +39,8 @@ def process(selection)
     print_footer
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
