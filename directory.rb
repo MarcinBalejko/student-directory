5bd975c92872e3a3f4646f69a1ff6dc0cc814381
MarcinBalejko <mbbm9@o2.pl>
2019-04-25 01:41:00 +0800

    Removed load from hardcoded file option

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 @students = []
 def save_students
   puts "Enter the name of the file:"
-  prompt_file = STDIN.gets.chomp         
+  prompt_file = STDIN.gets.chomp
   file = File.open(prompt_file, "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -18,11 +18,11 @@ def try_load_students
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else     #if it doesn't exists
-    puts "Sorry, #{filename} doesn't exist." ##########
-    exit #quit the program
+    puts "Sorry, #{filename} doesn't exist."
+    exit
   end
 end
-def load_students(filename = "students.csv")
+def load_students(filename)
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
@@ -30,8 +30,7 @@ def load_students(filename = "students.csv")
   end
   file.close
 end
-################################  so far it works
-def load_own_file
+def load_custom_file
   puts "Enter the name of the file to load:"
   own_file = STDIN.gets.chomp
   file = File.open(own_file, "r")
@@ -42,7 +41,6 @@ def load_own_file
   puts "Data successfully loaded from #{own_file}"
   file.close
 end
-####################################
 def interactive_menu
   loop do
     print_menu
@@ -53,9 +51,8 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to a file"
-  puts "4. Load the list from students.csv"
-  puts "5. Load the list from a file"
-  puts "6. Exit" # 9 because we'll be adding more items
+  puts "4. Load the list from a file"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 def process(selection)
   case selection
@@ -68,13 +65,11 @@ def process(selection)
   when "3"
     save_students
   when "4"
-    load_students
-  when "5"
-    load_own_file
-  when "6"
+    load_custom_file
+  when "9"
     exit
   else
-    puts "I don't know what you meant, try again"
+    puts "I don't know what you meant, please try again"
     end
   end
 def show_students
@@ -83,7 +78,7 @@ def show_students
   print_footer
 end
 def input_students
-  puts "Please enter the names of the students" ###########
+  puts "Please enter the names of the students"
   puts "Hit return twice to go back to the main menu"
 name = STDIN.gets.chomp
   puts "Please enter the cohort"
@@ -98,7 +93,7 @@ cohort_prompt = STDIN.gets.chomp
    puts "Please enter the name of the student"
    name = STDIN.gets.chomp
    puts "Please enter the cohort"
-   cohort_prompt = STDIN.gets.chomp ######################
+   cohort_prompt = STDIN.gets.chomp
  end
 @students.sort_by {|sym| sym[:cohort]}
 end
@@ -116,5 +111,5 @@ def print_footer
   puts "--------------------------------"
   puts "Overall, we have #{@students.count} great students."
 end
-try_load_students      #loads the list from the start
+try_load_students
 interactive_menu
